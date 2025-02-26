6a287e8500037271fcad2f9fdb5ef1797f41150c
hetu <patelh29@myumanitoba.ca>
2025-02-26 12:42:27 -0600

    part 2, added cycle functionality

--- planner_move_targets.py
+++ planner_move_targets.py
@@ -2,18 +2,21 @@ from planner import Planner
 from navigator import Navigator
 
 class PlannerMoveTargets(Planner):
-    def __init__(self, target):
+    def __init__(self, target, cycle):
         super().__init__()
         self._controller = None
         self._target_set = False
         self._target = target # tuple of target example use: PlannerMoveOnce( (300, 1, math.pi/2) )
         self._target_index = 0
+        self._cycle = cycle
+        self._cycle_index = 0
 
     def setup(self):
         # self.navigator = Navigator() # monitoring navigator
         # self._controller._navigator._target = self._target
         self._target_set = False
         self._target_index = 0
+        self._cycle_index = 0
         print("Planner finish setup!")
 
     def update(self):
@@ -25,6 +28,10 @@ class PlannerMoveTargets(Planner):
             self._controller._navigator.set_target(self._target[self._target_index])
             self._target_index += 1
         
+        if self._target_index == len(self._target) and self._cycle_index < self._cycle:
+            self._target_index = 0
+            self._cycle_index += 1
+        
         return self._target_index < len(self._target)
 
     def terminate(self):
@@ -32,4 +39,5 @@ class PlannerMoveTargets(Planner):
         self._target_set = False
         self._target = None
         self._target_index = 0
+        self._cycle_index = 0
         print("Planner terminated")
