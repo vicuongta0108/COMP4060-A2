2b9c9f4772d8a29f0c40204881ee79f84154a91c
hetu <patelh29@myumanitoba.ca>
2025-02-26 13:38:18 -0600

    task2 completed

--- as2_task2.py
+++ as2_task2.py
@@ -2,8 +2,8 @@ import math
 import robot_epuck, planner_move_targets, navigator_diff_simple, controller
 
 robot = robot_epuck.RobotEPuck("COM10")
-targets = ((200, 1, 0), (0, .2, math.pi/2))
-planner = planner_move_targets.PlannerMoveTargets( targets, 2 )
+targets = (  (200, 0.3, 0), (0, 0.1, math.pi/2),)
+planner = planner_move_targets.PlannerMoveTargets( targets, 4 )
 navigator = navigator_diff_simple.NavigatorDiffSimple()
 controller = controller.Controller()
 
--- navigator_diff_simple.py
+++ navigator_diff_simple.py
@@ -22,7 +22,7 @@ class NavigatorDiffSimple(Navigator):
         self.has_hit_target = True  # Reset target tracking
         self._last_l_steps = 0
         self._last_r_steps = 0
-        print("Navigator finish setup!")
+        # print("Navigator finish setup!")
 
     def update(self):
         # print(f'last left steps moved = {self._last_l_steps}')
@@ -35,10 +35,10 @@ class NavigatorDiffSimple(Navigator):
         state, _ = self._controller._robot.odom_update()
         self._left_steps_moved += abs(steps_delta(self._last_l_steps, state.sens_left_motor_steps))
         self._right_steps_moved += abs(steps_delta(self._last_r_steps, state.sens_right_motor_steps))
-        # print(f'last left steps moved = {self._last_l_steps}')
-        # print(f'last right steps moved = {self._last_r_steps}')
-        print(f'left steps moved = {self._left_steps_moved}')
-        print(f'right steps moved = {self._right_steps_moved}')
+        # print(f'left steps to moved = {self._speed_n_steps[2]}')
+        # print(f'right steps to moved = {self._speed_n_steps[3]}')
+        # print(f'left steps moved = {self._left_steps_moved}')
+        # print(f'right steps moved = {self._right_steps_moved}')
         if self._left_steps_moved < self._speed_n_steps[2] or self._right_steps_moved < self._speed_n_steps[3]:
             self.has_hit_target = False
             self._controller._robot._state.act_left_motor_speed = self._speed_n_steps[0]
@@ -55,12 +55,14 @@ class NavigatorDiffSimple(Navigator):
         self._speed_n_steps = None
         self._last_l_steps = 0
         self._last_r_steps = 0
-        print("Navigator terminated")
+        # print("Navigator terminated")
 
     # Use the given target tuple and A1 kinematics to solve how the robot should move.
     def set_target(self, target):
         # self.target_mm = diff_drive_inverse_kin(target)
-        actual_speed = target[1] * MAX_SPEED / 100
+        actual_speed = target[1] * MAX_SPEED
         self._speed_n_steps = diff_drive_inverse_kin(target[0], actual_speed, target[2])
+        self._left_steps_moved = 0
+        self._right_steps_moved = 0
         self.has_hit_target = False
         
