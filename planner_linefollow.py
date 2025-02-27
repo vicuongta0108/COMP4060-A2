3a7c51ceba4a044813e5bca951d681aa5c5f567e
Cuong Ta <cuongtavi.hcm@gmail.com>
2025-02-26 19:04:59 -0600

    task 3 planner done

--- planner_linefollow.py
+++ planner_linefollow.py
@@ -8,13 +8,11 @@ class PlannerLineFollow(Planner):
         super().__init__()
         self._controller = None
         self._last_error = 0
-        self._integral = 0
         self.sens_ground_prox = None
         self.THRESHOLD = 0 # Adjust this value to change the threshold for the line sensor
 
     def setup(self):
         self._last_error = 0
-        self._integral = 0
         self.sens_ground_prox = self._controller._robot._state.sens_ground_prox
         self.THRESHOLD = 500
 
@@ -39,11 +37,11 @@ class PlannerLineFollow(Planner):
         return error
     
     def compute_PID(self, error):
-        self._integral += self.get_line_position() # Accumulate the error
+        integral += self.get_line_position() # Accumulate the error
         derivative = error - self._last_error # Anticipate the next error
         self._last_error = error # Update the last error
 
-        output = (Kp * error) + (Ki * self._integral) + (Kd * derivative)
+        output = (Kp * error) + (Ki * integral) + (Kd * derivative)
         return output
 
     def update(self):
@@ -63,5 +61,4 @@ class PlannerLineFollow(Planner):
         self._controller._robot._state.stop_all()  # Stop robot motors
         self._controller = None
         self._last_error = 0
-        self._integral = 0
         self.sens_ground_prox = None
