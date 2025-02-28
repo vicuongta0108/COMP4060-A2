ce0d3e5cc8afd8002e33104cb806b5d23617e486
dipeshasd <dipesh.sah@hotmail.com>
2025-02-28 17:43:55 -0600

    CT/task4 add main

---
+++ as2_task4.py
@@ -0,0 +1,14 @@
+import math
+import robot_epuck, planner_find_follow_line, navigator_diff_direct_percent, controller
+
+robot = robot_epuck.RobotEPuck("COM10")
+targets = (0.2, 0.2)
+planner = planner_find_follow_line.PlannerFindFollowLine()
+navigator = navigator_diff_direct_percent.NavigatorDiffDirectPercent(targets)
+controller = controller.Controller()
+
+controller.setup(planner, navigator, robot)
+controller.start()  # blocking until done
+controller.terminate()
+
+print("Controller exited")
